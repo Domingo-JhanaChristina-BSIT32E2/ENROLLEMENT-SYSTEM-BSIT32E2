bfb06aec28fd8354a1b92bb67e129959fd1b93fd
Domingo-Jhana Christina L.-BSIT 32E2 <148880134+Domingo-JhanaChristina-BSIT32E2@users.noreply.github.com>
2024-06-30 23:03:32 +0800

    Add project files.

---
+++ ENROLLEMENT SYSTEM BSIT32E2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ENROLLEMENT_SYSTEM_BSIT32E2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
